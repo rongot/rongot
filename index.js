e0c0a7fae7195d51d82ba5d696904a5e2756ebcc
rongot <rgotliv@gmail.com>
2022-08-23 16:24:29 +0300

    Add files via upload
    simple app from Jenkins

---
+++ index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+const start = Date.now()
+app.get('/ping', function (req, res) {
+  res.send('Pong');
+});
+app.get('/status', function (req, res) {
+    const stop = Date.now()
+    var timer=`${(stop - start)/1000} seconds`;
+    res.send(JSON.stringify({serverUpTime: timer}))
+  });
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
