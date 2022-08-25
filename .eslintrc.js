8d1ae89a8afb44b5d19746bbe0a85a906e22c673
rgotliv <104550329+rgotliv@users.noreply.github.com>
2022-08-25 09:18:43 +0300

    update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,11 @@
-export default {
+module.exports ={
     "env": {
         "browser": true,
         "es2021": true
     },
     "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
+        'eslint:recommended',
+        'prettier',
     ],
     "overrides": [
     ],
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// import express from 'express';
 import express from 'express';
 var app = express();
 const start = Date.now()
